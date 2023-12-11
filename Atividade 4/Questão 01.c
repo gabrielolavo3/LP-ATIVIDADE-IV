f82a6977258676355c80925685a29b8b77df9a25
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-12-11 17:00:13 -0300

    Update Questão 01.c

--- Atividade 4/Questão 01.c
+++ Atividade 4/Questão 01.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <locale.h>
 
 #define G 3
 
 struct contato {
     char nome[250];
-    int numero_de_telefone;
+    char numero_de_telefone[11];
     char email[250];
 };
 
-int informacao (struct contato telefone[], char* busca_de_contato) {
+char* informacao (struct contato telefone[], char* busca_de_contato) {
+
     int a;
-
     for (a = 0; a < G; a++) {
-        if (strcmp(contato[a].nome, busca_de_contato) == 0) {
-
-        }
+        if (strcmp(telefone[a].nome, busca_de_contato) == 0) {
+            printf ("Número do contato: %s\n", telefone[a].numero_de_telefone);
+            break;
     }
+    printf ("OPS! O Contato não foi localizado!"); 
 }
 
 int main ( ) {
     setlocale (LC_ALL, "");
 
-    int a, busca_de_contato;
+    int a;
+    char busca_de_contato;
     struct contato telefone[G];
 
     for (a = 0; a < G; a++) {
         printf ("Digite o nome do %dº contato: ", a+1);
         gets (telefone[a].nome);
         printf ("Digite o número do contato: ");
-        scanf ("%d", &telefone[a].numero_de_telefone);
+        gets (telefone[a].numero_de_telefone);
         printf ("Digite um email: ");
         gets (telefone[a].email);
     }
@@ -38,7 +41,7 @@ int main ( ) {
     printf ("Informe o nome do contato que deseja buscar: ");
     gets (busca_de_contato);
 
-    contato(busca_de_contato, telefone);
+    informacao(telefone, busca_de_contato);
 
     return 0;
 }
